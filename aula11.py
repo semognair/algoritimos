12f9c9101d6d5b7ad6ae5d61935acc16813e3452
semognair <rian0001gomes@gmail.com>
2026-05-13 20:17:11 -0300

    Create aula11.py

---
+++ aula11.py
@@ -0,0 +1,33 @@
+"""
+
+print("o primeiro programa a gente nunca esquece")
+print("diga seu nome:")
+input()
+print("diga em que estado voce mora:")
+input()
+print("nome do seu municipio:")
+input()
+print("calsinha preta")
+"""
+"""
+print("digite um numero")
+a1= float(input())
+print("digite outro numero")
+a2= float(input())
+total= a1+a2
+print(total)
+
+print("digite um numero")
+a1= int(input())
+print("digite outro numero")
+a2= int(input())
+total= a1+a2
+print(total)
+"""
+num = int(input("digite um numero:"))
+antecessor = num -1
+sucessor = num +1
+
+print(f"antecessor:{antecessor}")
+print(f"sucessor:{sucessor}")
+
